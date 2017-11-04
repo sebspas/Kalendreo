f2eeb5d4d8f6fbd48e776dc7bde0f9cc014666d4
xkr0 <cros.camille31@gmail.com>
2017-11-04 17:34:01 -0400

    get and display the selected day in calendar

--- app/src/main/java/com/example/sebsp/kalendreo/CalendarView.java
+++ app/src/main/java/com/example/sebsp/kalendreo/CalendarView.java
@@ -2,11 +2,15 @@ package com.example.sebsp.kalendreo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalendarView extends AppCompatActivity {
 
@@ -24,6 +28,17 @@ public class CalendarView extends AppCompatActivity {
                 startActivity(new Intent(CalendarView.this, CreateEventActivity.class));
             }
         });
-    }
 
+        // display date of the selected day
+        final android.widget.CalendarView calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new android.widget.CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull android.widget.CalendarView calendarView, int year, int month, int day) {
+                TextView txt = findViewById(R.id.no_event);
+                txt.setText(year + "/" + month + "/" + day);
+            }
+        });
+
+    }
 }
