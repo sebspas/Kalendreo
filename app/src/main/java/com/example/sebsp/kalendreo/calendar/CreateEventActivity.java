aef490de251a241a4b967e017047febedc09400e
GaetanMartin <gaetan.martin1993@gmail.com>
2017-11-18 11:36:01 -0500

    Removed useless imports

--- app/src/main/java/com/example/sebsp/kalendreo/calendar/CreateEventActivity.java
+++ app/src/main/java/com/example/sebsp/kalendreo/calendar/CreateEventActivity.java
@@ -1,25 +1,12 @@
 package com.example.sebsp.kalendreo.calendar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.example.sebsp.kalendreo.AbstractLoggedInActivity;
-import com.example.sebsp.kalendreo.MainActivity;
 import com.example.sebsp.kalendreo.R;
 import com.example.sebsp.kalendreo.components.EventViewGroup;
-import com.example.sebsp.kalendreo.model.AllEvents;
 import com.example.sebsp.kalendreo.model.Event;
-import com.example.sebsp.kalendreo.model.pojo.EventPOJO;
-import com.example.sebsp.kalendreo.utils.ReminderManager;
-import com.example.sebsp.kalendreo.utils.Tag;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class CreateEventActivity extends AbstractLoggedInActivity {
 
