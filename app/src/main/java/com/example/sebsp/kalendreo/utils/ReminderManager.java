ad600e7491564a237e4a2bb75f6cca5f318460ab
xkr0 <cros.camille31@gmail.com>
2017-11-29 20:41:48 -0500

    fixing reminders

--- app/src/main/java/com/example/sebsp/kalendreo/utils/ReminderManager.java
+++ app/src/main/java/com/example/sebsp/kalendreo/utils/ReminderManager.java
@@ -4,8 +4,11 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.example.sebsp.kalendreo.model.Event;
+
+import java.util.Calendar;
 
 import static android.content.Context.ALARM_SERVICE;
 
@@ -36,17 +39,28 @@ public class ReminderManager {
 
     private static void setReminders(AlarmManager alarmManager, Event e, PendingIntent pendingIntent) {
 
+        Calendar calendar = e.getStartDate();
+        // start date of the event
+        Calendar eDate = e.getStartDate();
+        int min = eDate.get(Calendar.MINUTE);
+        int hour = eDate.get(Calendar.HOUR_OF_DAY);
+        int day = eDate.get(Calendar.DAY_OF_MONTH);
+        int month = eDate.get(Calendar.MONTH) +1;   // 0 based months !
+        int year = eDate.get(Calendar.YEAR);
 
         //Setting alarm for the given time
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, e.getStartDate().getTimeInMillis(), pendingIntent);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, eDate.getTimeInMillis(), pendingIntent);
 
         // TODO set various reminders according to the event category
-//        if (e.categorie.equals("Fun")){
-//            // check the correctness of the date for a 2-hour-early reminder
-//            int checkedDate[] = TimeBoundsChecker.checkForHour(2, hour, day, month, year);
-//            calendar.set(checkedDate[3], checkedDate[2], checkedDate[1], checkedDate[0], min, 00);
-//
-//            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-//        }
+       if (e.getCategory().equals("Fun")){
+           Log.d("FUN:::::", "hellloooooooooooooooo");
+            // check the correctness of the date for a 2-hour-early reminder
+            int checkedDate[] = TimeBoundsChecker.checkForHour(2, hour, day, month, year);
+            Log.d("BEFORE:::::", String.valueOf(calendar.get(Calendar.HOUR_OF_DAY)));
+            calendar.set(checkedDate[3], checkedDate[2], checkedDate[1], checkedDate[0], min, 00);
+            Log.d("AFTER:::::", String.valueOf(calendar.get(Calendar.HOUR_OF_DAY)));
+
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        }
     }
 }
